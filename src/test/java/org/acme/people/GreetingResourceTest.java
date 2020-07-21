8314eccf1250a34e7bc3d7985c8b92c51e2a72fe
chaika <chaika@localhost.localdomain>
2020-07-21 11:44:44 -0400

    first commit

--- src/test/java/org/acme/people/GreetingResourceTest.java
+++ src/test/java/org/acme/people/GreetingResourceTest.java
@@ -21,4 +21,14 @@ public class GreetingResourceTest {
     }
 
     // add more tests
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+          .pathParam("name", uuid)
+          .when().get("/hello/greeting/{name}")
+          .then()
+            .statusCode(200)
+            .body(startsWith("hello " + uuid));
+    }
 }
